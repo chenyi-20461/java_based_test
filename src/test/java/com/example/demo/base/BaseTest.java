9f92824e5c12bdfea95d190776a19941e65352ef
chenYi <363874537@qq.com>
2020-12-08 16:42:20 +0800

    BaseTest

--- src/test/java/com/example/demo/base/BaseTest.java
+++ src/test/java/com/example/demo/base/BaseTest.java
@@ -3,13 +3,31 @@ package com.example.demo.base;
 import org.junit.jupiter.api.Test;
 
 public class BaseTest {
-//    测试||只判前面就能行
+    //    测试||和&&只判前面就能行,
     @Test
     public void testIf() {
         String a = null;
         String b = null;
-        if (a == null || b.toString() == "1") {
+        if (a == null || a.toString() == "1") {
             System.out.println(1);
+        }
+
+        if (a != null && b.toString() == "1") {
+            System.out.println(1);
+        }
+    }
+
+    //    测试while快捷写法
+    @Test
+    public void testWhile() {
+        int n = 3;
+        while (n-- != 0) {
+            System.out.println(n);
+        }
+        int a = 3;
+        while (a != 0) {
+            a--;
+            System.out.println(a);
         }
     }
 
--- src/test/java/com/example/demo/base/StringTest.java
+++ src/test/java/com/example/demo/base/StringTest.java
@@ -1,11 +1,23 @@
 package com.example.demo.base;
 
-import org.junit.jupiter.api.Test;
+        import org.junit.jupiter.api.Test;
 
 public class StringTest {
-//   判断字符串保存另一个字符串
+    //   判断字符串保存另一个字符串
     @Test
-    public void test1(){
+    public void test1() {
         System.out.println("abc".contains("ab"));
     }
+
+    //    判断字符串是否等于数字
+    @Test
+    public void test2() {
+        System.out.println("2".equals(2));
+    }
+    @Test
+    public void test3() {
+        Object a="1";
+        System.out.println(a instanceof String);
+    }
+
 }
