d354f68e82721c381459728e384cd62e7b7bc466
chenYi <363874537@qq.com>
2020-12-14 16:03:51 +0800

    lombok的test

--- src/test/java/com/example/demo/junit/constructor.java
+++ src/test/java/com/example/demo/junit/constructor.java
@@ -1,20 +1,20 @@
 package com.example.demo.junit;
 
 import com.example.demo.model.Person;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Test;
 
 /**
  * Junti不能在有两个构造方法的类中使用
  */
-@NoArgsConstructor
-@AllArgsConstructor
+//@NoArgsConstructor
+//@AllArgsConstructor
 public class constructor {
     /**
      * 报错.
      * org.junit.platform.commons.PreconditionViolationException: Class
      * [com.example.demo.optional.OptionalTest] must declare a single constructor
+     *
+     * Error:(12, 1) java: 已在类 com.example.demo.junit.constructor中定义了构造器 constructor()
      */
     @Test
     public void test1() {
--- src/test/java/com/example/demo/optional/OptionalTest.java
+++ src/test/java/com/example/demo/optional/OptionalTest.java
@@ -1,24 +1,31 @@
 package com.example.demo.optional;
 
 import com.example.demo.model.Person;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+import java.util.Optional;
 
-@NoArgsConstructor
-@AllArgsConstructor
+/**
+ * 测试optional
+ */
 public class OptionalTest {
     private String name;
 
-        /**
+    /**
      * 优化空指针.
      */
     @Test
     public void test1() {
         Person person = new Person();
         person.setAge(12);
-        System.out.println(person.getAge());
+        if (Objects.nonNull(person)) {
+            System.out.println(person.getAge());
+        }
+//        优化
+        Optional<Person> optionalPerson = Optional.ofNullable(person);
+        Integer name = optionalPerson.map(a -> a.getAge()).get();
+        System.out.println(name);
     }
 
 }
