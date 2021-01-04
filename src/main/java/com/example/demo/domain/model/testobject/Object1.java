4c3c5bd1f3d8da43eb8d7249af97e0bf4e4b4984
chenYi <363874537@qq.com>
2021-01-04 20:02:44 +0800

    hashcode和基本类的使用

---
+++ src/main/java/com/example/demo/domain/model/testobject/Object1.java
@@ -0,0 +1,8 @@
+package com.example.demo.domain.model.testobject;
+
+public class Object1 {
+    @Override
+    public int hashCode() {
+        return 1884122755;
+    }
+}
---
+++ src/main/java/com/example/demo/domain/model/testobject/Object2.java
@@ -0,0 +1,8 @@
+package com.example.demo.domain.model.testobject;
+
+public class Object2 {
+    @Override
+    public int hashCode() {
+        return 1884122755;
+    }
+}
