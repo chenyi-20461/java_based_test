8d8267f5ba06747c7cdb2ec4f5164e5f0ff55514
chenYi <363874537@qq.com>
2021-01-14 19:09:02 +0800

    枚举的测试

---
+++ src/test/java/com/example/demo/enumtest/EnumTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.enumtest;
+
+import com.example.demo.constant.TestEnum;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 枚举测试类.
+ */
+public class EnumTest {
+    /**
+     * 测试输出的字符串，最干脆的定义，无属性
+     */
+    @Test
+    public void test1(){
+        System.out.println(TestEnum.FANG_XUN_TONG);
+        System.out.println(TestEnum.lowString);
+    }
+
+    /**
+     * valueOf
+     * 没有键会抛异常
+     */
+    @Test
+    public void test2(){
+        System.out.println(TestEnum.valueOf("1"));
+    }
+}
