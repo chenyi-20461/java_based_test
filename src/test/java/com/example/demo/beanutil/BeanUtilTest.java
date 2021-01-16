589a245cc2c3b6f2665bbbf4205a8be3fd3b8b05
chenYi <363874537@qq.com>
2021-01-16 10:03:38 +0800

    json复制数组

---
+++ src/test/java/com/example/demo/beanutil/BeanUtilTest.java
@@ -0,0 +1,42 @@
+package com.example.demo.beanutil;
+
+import com.alibaba.fastjson.JSON;
+import com.example.demo.domain.model.Person;
+import com.example.demo.domain.model.compare.Dog;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeanUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeanUtilTest {
+    /**
+     * 看能否复制null
+     *
+     * 不能复制null
+     */
+    @Test
+    public void test(){
+        List list = new ArrayList();
+        BeanUtils.copyProperties(null,list);
+        System.out.println(list);
+    }
+
+    /**
+     * 能否复制list.
+     * 不能
+     */
+    @Test
+    public void testOne(){
+        List<Dog> list = new ArrayList<>();
+        Dog dog = new Dog();
+        dog.setName("zhangSan");
+        list.add(dog);
+        List<Dog> list1= new ArrayList<Dog>();
+        BeanUtils.copyProperties(list,list1);
+        System.out.println(list1);
+//        解决办法
+        System.out.println(JSON.parseArray(JSON.toJSONString(list), Person.class));
+    }
+
+}
