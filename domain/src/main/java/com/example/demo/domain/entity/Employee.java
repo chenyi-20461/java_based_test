1d6e8b72dbabc99b496836398b6ef05187c0299c
chenYi <363874537@qq.com>
2021-02-24 15:48:24 +0800

    springBoot 添加mybatis自动生成插件

---
+++ domain/src/main/java/com/example/demo/domain/entity/Employee.java
@@ -0,0 +1,40 @@
+package com.example.demo.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "test_employee")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private String age;
+
+    @Column(name = "del_flag")
+    private Integer delFlag;
+
+    @Column(name = "address")
+    private String address;
+}
